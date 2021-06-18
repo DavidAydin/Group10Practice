0aaa35eaa907589878d422f89cd26f9012f58f06
DavidAydin <tarikaydin@gmail.com>
2021-06-18 19:52:07 +0200

    TC_007 created

---
+++ src/test/java/TC_007.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_007 {
+
+    @Test
+    public void TestLoginFunction(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.amazon.de");
+
+    }
+}
