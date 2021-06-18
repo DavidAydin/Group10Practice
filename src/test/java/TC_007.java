bc84f4aacfe6eda29dbf33e17e41d9a10d55424e
DavidAydin <tarikaydin@gmail.com>
2021-06-18 20:58:38 +0200

    TC_007 updated by David second time

--- src/test/java/TC_007.java
+++ src/test/java/TC_007.java
@@ -11,6 +11,7 @@ public class TC_007 {
         WebDriver driver = new ChromeDriver();
 
         driver.navigate().to("https://www.amazon.de");
+        driver.navigate().back();
 
 
         driver.quit();
