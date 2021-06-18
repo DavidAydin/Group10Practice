b988501463c9f96a164db57a6e3285ba0ace240b
SofuHalid <sofuhalid@gmail.com>
2021-06-18 20:05:08 +0200

    feature-4 initial

---
+++ src/test/java/TC_004.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_004 {
+
+    WebDriver driver;
+
+    @Test
+    public void test_case_004(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.cybertekschool.com/");
+    }
+
+}
